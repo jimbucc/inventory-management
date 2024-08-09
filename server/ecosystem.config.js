9336c1f576e1d2815de752d62bf7f7358902c404
jimbucc <James.Bucciferro@PERKINELMER.COM>
2024-08-08 20:55:51 -0400

    fixed formatting

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,11 +1,13 @@
-module.export = {
-    apps: {
-        name: "inventory-management",
-        scripts: "npm",
-        args: "run dev",
-        env: {
-            NODE_ENV: "development",
-            ENV_VAR1: "environment-variable"
-        }
-    }
+module.exports = {
+  apps: [
+    {
+      name: 'inventory-management',
+      scripts: 'npm',
+      args: 'run dev',
+      env: {
+        NODE_ENV: 'development',
+        ENV_VAR1: 'environment-variable',
+      },
+    },
+  ],
 }
